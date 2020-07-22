d0a509b92c03a4bbd2656fb5ea6132bbb465cabe
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-22 20:37:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,13 @@
 class Dog
- 
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-    if dogs_name
-      puts "woof!"
+    def name=(name)
+        @name = name
     end
-  end
- 
-  def name
-    @this_dogs_name
-  end
+    
+    def name
+        @name
+    end
+    
+    def bark
+        puts "woof!"
+    end
 end
- 
-lassie = Dog.new
-lassie.name = "Lassie"
- 
-puts lassie.name
